9f990f899606d14d1d554d9f7d17aeaf975912e4
Uroshek <zaletelj.uros@gmail.com>
2015-12-19 15:46:23 +0100

    added Location model

---
+++ TuristFlow/TuristFlow/models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristFlow.models
+{
+    class Location
+    {
+        string LAT { get; set; }
+        string LOT { get; set; }
+        string City { get; set; }
+        string Types { get; set; }
+        int Radius { get; set; }
+    }
+}
