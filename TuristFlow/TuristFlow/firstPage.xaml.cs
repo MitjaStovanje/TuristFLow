7d726be0041af5137670bc0ab922335f0a59a3b6
Mitja <mitja.pitko@gmail.com>
2015-12-19 01:28:55 +0100

    add model map and person model.

--- TuristFlow/TuristFlow/firstPage.xaml.cs
+++ TuristFlow/TuristFlow/firstPage.xaml.cs
@@ -26,5 +26,6 @@ namespace TuristFlow
         {
             this.InitializeComponent();
         }
+
     }
 }
---
+++ TuristFlow/TuristFlow/models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuristFlow.models
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public int Age { get; set; }
+        public string groupt { get; set; }
+        public string sleeping { get; set; }
+        public int TravelTime { get; set; }
+        public bool Budget { get; set; }
+        public string Transport { get; set; }
+
+    }
+}
